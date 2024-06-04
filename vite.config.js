3c595d87e0bf1f687165aa624983fcbcbf5d0571
Jon White <jonw@cogapp.com>
2024-06-04 11:38:32 +0100

    Switch to Action for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 
+
+// output js and css files to dist folder, not dist/assets
+// Use relative paths for assets
 export default defineConfig({
     server: {
         port: 3000,
+    },
+    base: '',
+    build: {
+        outDir: 'dist',
+        assetsDir: './',
     },
 })
